592e5d26873eb887d7db9b3550712de08b948400
keanna <keannamalien@gmail.com>
2024-06-28 16:00:56 -0700

    checkWinner working

--- script/script.js
+++ script/script.js
@@ -6,22 +6,23 @@ const gameTable = document.querySelector(".game-container");
 const winScreen = document.querySelector(".game-win");
 const winMessage = document.querySelector(".message");
 const restartBtn = document.querySelector(".restart-btn");
-const cells = document.querySelectorAll(".cell");
+const cells = Array.from(document.querySelectorAll(".cell"));
 
-const winPatterns = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
+// const winPatterns = [
+//   [0, 1, 2],
+//   [3, 4, 5],
+//   [6, 7, 8],
+//   [0, 3, 6],
+//   [1, 4, 7],
+//   [2, 5, 8],
+//   [0, 4, 8],
+//   [2, 4, 6],
+// ];
 
 let spaces = ["", "", "", "", "", "", "", "", ""];
 let currentPlayer = "X";
 let isRunning = false;
+let gameWon = false;
 
 function startGame() {
   startScreen.classList.add("hide");
@@ -49,6 +50,7 @@ function cellClicked() {
   //   element.style.pointerEvents = "none"; // so user can't click!
   //   gameTable.style.pointerEvents = "none";
   //   let botDelay =
+  vsComputer();
 }
 
 function placeSymbol(cell, index) {
@@ -57,11 +59,61 @@ function placeSymbol(cell, index) {
 }
 
 function changePlayer() {
-  currentPlayer = currentPlayer == "X" ? vsComputer() : "X";
+  currentPlayer = currentPlayer == "X" ? "O" : "X";
 }
 
-function vsComputer() {}
+function vsComputer() {
+  let botOptions = []; //create an empty array
+  for (let i = 0; i < cells.length; i++) {
+    // figure out which cells are empty then push to the array
+    if (cells[i].textContent === "") {
+      botOptions.push(cells[i]);
+    }
+    console.log(botOptions);
+  }
+  const emptyIndex = Math.floor(Math.random() * botOptions.length); //generate random index from the array
+  console.log(emptyIndex);
+  placeSymbol(currentPlayer); //place O into selected cell
+  //if game is running changePlayer()
+}
 
-// function checkWinner {
+function checkWinningRow(cell1, cell2, cell3, symbol) {
+  if (
+    cell1.textContent === symbol &&
+    cell2.textContent === symbol &&
+    cell3.textContent === symbol
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-// }
+function checkWinner() {
+  const isTrue = (currentValue) => currentValue.textContent !== "";
+  if (
+    checkWinningRow(cells[0], cells[1], cells[2], currentPlayer) ||
+    checkWinningRow(cells[3], cells[4], cells[5], currentPlayer) ||
+    checkWinningRow(cells[6], cells[7], cells[8], currentPlayer) ||
+    checkWinningRow(cells[0], cells[3], cells[6], currentPlayer) ||
+    checkWinningRow(cells[1], cells[4], cells[7], currentPlayer) ||
+    checkWinningRow(cells[2], cells[5], cells[8], currentPlayer) ||
+    checkWinningRow(cells[0], cells[4], cells[8], currentPlayer) ||
+    checkWinningRow(cells[2], cells[4], cells[6], currentPlayer)
+  ) {
+    gameWon = true;
+    isRunning = false;
+    cells.forEach((cell) => {
+      cell.style.pointerEvents = "none";
+    });
+    gameTable.style.pointerEvents = "none";
+    gameTable.classList.remove("show");
+    winScreen.classList.add("show");
+  } else {
+    if (cells.every(isTrue)) {
+      gameTable.classList.remove("show");
+      winScreen.classList.add("show");
+      console.log("game is a draw");
+    }
+  }
+}
