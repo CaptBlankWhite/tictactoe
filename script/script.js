7fb1763f3faf236d543eab7169306f5bc16b24fb
keanna <keannamalien@gmail.com>
2024-06-28 00:55:34 -0700

    Styled win-message

--- script/script.js
+++ script/script.js
@@ -1,1 +1,12 @@
 "use strict";
+
+const startScreen = document.querySelector(".start-screen");
+const startBtn = startScreen.querySelector(".start-container .start-btn");
+const gameTable = document.querySelector(".game-container");
+
+function startGame() {
+  startScreen.classList.add("hide");
+  gameTable.classList.add("show");
+}
+
+startBtn.addEventListener("click", startGame);
